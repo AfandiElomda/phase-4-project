707758671224f51c942a26fc1e34768934d88931
+ <+>
2022-12-05 23:53:00 +0300

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :orders
+  resources :orders, only: [:create]
   # resources :users
-  resources :groceries
+  resources :groceries, only: [:index, :show, :destroy, :update]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
