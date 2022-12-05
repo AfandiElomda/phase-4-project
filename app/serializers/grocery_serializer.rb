fe69bb4ca54917113ca22cf44604fd51e41e37bb
+ <+>
2022-12-05 22:36:53 +0300

    updated serializers

--- app/serializers/grocery_serializer.rb
+++ app/serializers/grocery_serializer.rb
@@ -1,3 +1,3 @@
 class GrocerySerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :name, :image_url, :price, :rating
 end
--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,3 +1,3 @@
 class OrderSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :total, :user_id, :grocery_id
 end
