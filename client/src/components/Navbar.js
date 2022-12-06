88532408456bc9a4baf805c257f170b121acaf38
+ <+>
2022-12-06 12:40:44 +0300

    tailwind

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import { GiFruitBowl} from  "react-icons/hi2";
 
 const Navbar = () => {
   return (
     <div className='navbar'>
+      
       <Link to="/">
       <h3>Logo</h3>
     </Link>
