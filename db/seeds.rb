9e27e8a6a3e1322f21111c28f2a7dfec3bbb5854
+ <+>
2022-12-06 18:43:33 +0300

    seeds updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,14 +9,92 @@ puts "seeding "
 
 grocery_1 = Grocery.create(
     name:"",
-    image_url:"", price:20.2)
-grocery_2 = Grocery.create(name:"ffff1", price:203.2)
+    image_url:"",
+    price:,
+    rating:
+)
+grocery_2 = Grocery.create(
+    name:"", 
+    image_url:"",
+    price:,
+   rating:
+)
+grocery_3 = Grocery.create(
+    name:"", 
+    image_url:"",
+    price:,
+   rating:
+)
+grocery_4 = Grocery.create(
+    name:"", 
+    image_url:"",
+    price:,
+   rating:
+)
+grocery_5 = Grocery.create(
+    name:"", 
+    image_url:"",
+    price:,
+   rating:
+)
+grocery_6 = Grocery.create(
+    name:"", 
+    image_url:"",
+    price:,
+   rating:
+)
+grocery_7 = Grocery.create(
+    name:"", 
+    image_url:"",
+    price:,
+   rating:
+)
+grocery_8 = Grocery.create(
+    name:"", 
+    image_url:"",
+    price:,
+   rating:
+)
+grocery_9 = Grocery.create(
+    name:"", 
+    image_url:"",
+    price:,
+   rating:
+)
+grocery_10 = Grocery.create(
+    name:"", 
+    image_url:"",
+    price:,
+   rating:
+)
 
-user_1 = User.create(username:"sdsd",bio:"asdsjaqwjqwhqwqwqwqwwq")
-user_1 = User.create(username:"sdsdsd",bio:"asdsjaqwjqwhqwqwqwqqq123qwwq")
+user_1 = User.create(
+    username:"",
+    password:""
+)
+user_2 = User.create(
+    username:"",
+    password:""
+)
+user_3 = User.create(
+    username:"",
+    password:""
+)
+user_4 = User.create(
+    username:"",
+    password:""
+)
+user_5 = User.create(
+    username:"",
+    password:""
+)
 
-order_1 = Order.create(total:1224.345, user_id:1,grocery_id:1)
-order_2 = Order.create(total:1224.345, user_id:2,grocery_id:2)
+
+order_1 = Order.create(total:, user_id:1,grocery_id:1)
+order_2 = Order.create(total:, user_id:2,grocery_id:2)
+order_3 = Order.create(total:, user_id:2,grocery_id:2)
+order_4 = Order.create(total:, user_id:2,grocery_id:2)
+order_5 = Order.create(total:, user_id:2,grocery_id:2)
 
 
 
