d6233923f5386789203ec443c6e4982442e3a8fc
+ <+>
2022-12-06 13:35:22 +0300

    deleted client

--- client/src/components/index.js
+++
@@ -1,7 +0,0 @@
-export {default as Login} from './Login';
-export {default as SignUp} from './SignUp';
-export {default as Navbar} from './Navbar';
-export {default as Groceries} from './Groceries';
-export {default as Home} from './Home';
-export {default as Logo} from './Logo';
-export {default as Cart} from './Cart'
