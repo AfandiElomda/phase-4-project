57261e52187f1d5cd906f305d08789ea70377bc0
+ <+>
2022-12-07 18:17:37 +0300

    package.lock bugs

---
+++ client/src/components/Card.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Card = (props) => {
+  return (
+    <div className='card'>
+        {props.image_url}
+    </div>
+  )
+}
+
+export default Card
--- client/src/components/OurGroceries.js
+++ client/src/components/OurGroceries.js
@@ -1,10 +1,6 @@
 import React, {useState,useEffect} from 'react'
 import './OurGroceries.css'
-// import img1 from '../assets/marisol-benitez-QvkAQTNj4zk-unsplash.jpg'
-// import img2 from '../assets/no-revisions-ixS7UCRJTdM-unsplash.jpg'
-// import img3 from  '../assets/scott-warman-NpNvI4ilT4A-unsplash.jpg'
-// import img4 from  '../assets/tara-clark-Gk8LG7dsHWA-unsplash.jpg'
-// import img5 from  '../assets/woman-6289052_1280.png'
+import Card from './Card'
 
 const Groceries = () => {
   
@@ -16,29 +12,24 @@ const Groceries = () => {
       .then(data => setGrocery(data))
   }, []);
 
+  const groceryData = grocery.map((item) =>{
+    return  (
+      <Card
+      key={item.id}
+      name={item.name}
+      image_url={item.image_url}
+      price={item.price}
+      rating={item.rating}
+      />
+
+    ) }
+  )
  
   return (
     <div className='groceries'>
         <h1>Our Groceries</h1>
-         {grocery.map((item) => (
-          <div className='container'>
-             key={item.id} 
-             name={item.name}
-             image_url{item.image_url}
-             price={item.price}
-             rating={item.rating}
-          </div>
-             )
-         )}
-
-
-        {/* <div className='container'>
-          <img src={img1} alt=''/>
-          <img src={img2} alt=''/>
-          <img src={img3} alt=''/>
-          <img src={img4} alt=''/>
-          <img src={img5} alt=''/>
-        </div> */}
+        
+         {groceryData}
 
     </div>
   )
